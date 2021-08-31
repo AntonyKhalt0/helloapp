a8168ac510a0d59ea1c943fd456f993fd21cf658
AntonyKhalt0 <toscha.xalturin@gmail.com>
2021-08-31 22:29:03 +0300

    Change Action_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
 	protect_from_forgery with: :exception
 
 	def hello
-		render plain: "Hello, world!"
+		render plain: “¡Hola, mundo!”
 	end
 end
